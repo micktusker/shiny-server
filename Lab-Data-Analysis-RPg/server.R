faa93a9e08af6f801c99407ccadca180ae2faead
micktusker <micktusker@gmail.com>
2017-03-15 12:34:25 +0000

    fixed server.R

--- Lab-Data-Analysis-RPg/server.R
+++ Lab-Data-Analysis-RPg/server.R
@@ -1,8 +1,10 @@
 shinyServer(function(input, output) {
   statement <- reactive({
     sql <- "SELECT * FROM lookups.tcga_cancer_codes"
-    df <- dbFetch(dbSendQuery(pg.conn, statement=statement()), n=1000)
-    df
+    rs <- dbSendQuery(pg.conn, sql)
+    df <- data.frame(dbFetch(rs))
+    dbClearResult(rs)
+    return(df)
   })
-  output$result <- renderTable(df)
+  output$result <- renderTable(statement())
 })
