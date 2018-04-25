c4605c9f5ab7e957f313cc98b40b43b0a8bc4cba
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2018-04-25 15:50:02 +0100

    Added HTML documentation

--- AbCDR_Viewer/ui.R
+++ AbCDR_Viewer/ui.R
@@ -1,6 +1,16 @@
 library(shiny)
 fluidPage(
   titlePanel("Antibody CDR Analysis"),
+  tags$div(HTML(
+    "<h2>Purpose</h2>
+    <p>Locate comlementarity determining regions (CDR) in antibody variable region (VR) sequences</p>
+    <h2>Assumes:</h2>
+    <ul>
+      <li>Antibody VR sequence is human.</li>
+      <li>It contains <b>two cysteine</b> residues.</li>
+      <li>The sequence ends in either <b>EIK</b> for Light chain or <b>TVSS</b> for Heavy chain.</em>
+    </ul>")
+  ),
   textAreaInput("aa_seq", "Paste Sequence", width = "1000px", height = "100px"),
   submitButton(text = "Submit", icon = NULL),
   mainPanel(
