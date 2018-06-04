66e0c45f60b7db1b38fcbbb80cd79e35bf5c457c
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2018-06-04 16:25:59 +0100

    Changes to schema and UDFs

--- antibody_db/antibody_udfs.sql
+++ antibody_db/antibody_udfs.sql
@@ -1,206 +1,3 @@
-CREATE OR REPLACE FUNCTION antibodies.add_new_antibody_sequence(p_antibody_name TEXT,
-                                                                p_aa_sequence TEXT,
-                                                                p_extra_metadata TEXT DEFAULT '{}')
-RETURNS JSONB
-AS
-$$
-DECLARE
-  l_aa_sequence TEXT := REGEXP_REPLACE(UPPER(p_aa_sequence), '\s', '');
-  l_aa_sequence_id TEXT := MD5(l_aa_sequence);
-  l_antibody_name TEXT := TRIM(UPPER(p_antibody_name));
-  l_aa_sequence_metadata_id TEXT := MD5(CONCAT(l_aa_sequence_id, l_antibody_name));
-  l_extra_metadata JSONB := CAST(p_extra_metadata AS JSONB);
-  l_retval TEXT := '{"New Antibody Entry Created": "%s", ' || 
-  '"New Meta Data Entry Created": "%s", ' ||
-  '"Load Date": "%s", ' ||
-  '"Account": "%s", ' ||
-  '"Sequence ID": "%s", ' ||
-  '"Meta Data ID": "%s"}';
-  l_positive_outcome TEXT := 'YES';
-  l_negative_outcome TEXT := 'NO';
-  l_err_retval TEXT := '{"ERROR": "%s", ' ||
-  '"Load Date": "%s", ' ||
-  '"Account": "%s", ' ||
-  '"Antibody Name": "%s", ' ||
-  '"Antibody Sequence": "%s"}';
-l_sqlerrm TEXT;
-BEGIN
-  l_retval := FORMAT(l_retval, '%s', '%s', CURRENT_TIMESTAMP, '%s', '%s', '%s');
-  l_retval := FORMAT(l_retval, '%s', '%s', CURRENT_USER, '%s', '%s');
-  l_retval := FORMAT(l_retval, '%s', '%s', l_aa_sequence_id, '%s');
-  l_retval := FORMAT(l_retval, '%s', '%s', l_aa_sequence_metadata_id);
-  BEGIN
-    INSERT INTO antibodies.aa_sequences(aa_sequence_id, aa_sequence)
-      VALUES(l_aa_sequence_id, l_aa_sequence);
-    l_retval := FORMAT(l_retval, l_positive_outcome, '%s');
-  EXCEPTION
-    WHEN UNIQUE_VIOLATION THEN
-      l_retval := FORMAT(l_retval, l_negative_outcome, '%s');
-  END;
-  BEGIN
-    INSERT INTO antibodies.aa_sequence_metadata(aa_sequence_metadata_id, antibody_name, aa_sequence_id, extra_metadata)
-      VALUES(l_aa_sequence_metadata_id, l_antibody_name, l_aa_sequence_id, l_extra_metadata);
-    l_retval := FORMAT(l_retval, l_positive_outcome);
-  EXCEPTION
-    WHEN UNIQUE_VIOLATION THEN
-      l_retval := FORMAT(l_retval, l_negative_outcome);
-  END;
-
-  RETURN l_retval::JSONB;
-
-EXCEPTION
-  WHEN OTHERS THEN
-    l_sqlerrm := SQLERRM;
-    l_sqlerrm := REPLACE(l_sqlerrm, '"', '|');
-    l_err_retval := (FORMAT(l_err_retval, 
-                        l_sqlerrm, 
-                        CURRENT_TIMESTAMP,
-                        CURRENT_USER,
-                        p_antibody_name,
-                        p_aa_sequence))::JSONB;
-
-  RETURN l_err_retval::JSONB;
-
-END;
-$$
-LANGUAGE plpgsql
-SECURITY DEFINER;
-
-SELECT * FROM antibodies.add_new_antibody_sequence('test ab 1', 'EVQLLESGGGLVQPGGSLRLSCAASGFTFSSYGMSWVRQAPGKGLELVSTINGYGDTTYYPDSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCARDRDYGNSYYYALDYWGQGTLVTVSS');
-
-CREATE OR REPLACE FUNCTION antibodies.get_fasta_sequences_for_imgt_ids(p_imgt_ids TEXT[])
-RETURNS TEXT[]
-AS
-$$
-DECLARE
-  l_fasta_seqs TEXT[];
-  l_counter INTEGER := 1;
-  l_response TEXT;
-  imgt_id TEXT;
-BEGIN
-  FOREACH imgt_id IN ARRAY p_imgt_ids
-  LOOP
-    BEGIN
-	    l_response := bix_udfs.get_fasta_seq_as_aa_for_gene_synonym(imgt_id);
-	    l_response := CONCAT(imgt_id, E'\t', l_response);
-	    l_fasta_seqs[l_counter] := l_response;
-	    l_counter := l_counter + 1;
-	  EXCEPTION
-	    WHEN OTHERS THEN
-	      l_response := CONCAT(imgt_id, E'\t', 'ERROR');
-	      l_fasta_seqs[l_counter] := l_response;
-	      l_counter := l_counter + 1;
-	  END;
-  END LOOP;
-  
-  RETURN l_fasta_seqs;
-  
-END;
-$$
-LANGUAGE plpgsql
-STABLE
-SECURITY DEFINER;
-SELECT antibodies.get_fasta_sequences_for_imgt_ids(ARRAY['IGHV4-34', 'IGHV3-21', 'IGHV1-2']);
-
-CREATE OR REPLACE FUNCTION antibodies.get_fasta_sequencesimgt_table()
-RETURNS TABLE(imgt_gene_name TEXT, fasta_sequence TEXT)
-AS
-$$
-DECLARE
-  l_imgt_gene_names TEXT[];
-BEGIN
-  SELECT 
-    ARRAY_AGG(lu.imgt_gene_name) INTO l_imgt_gene_names 
-  FROM
-    antibodies.germline_adimab_imgt_lu lu;
-  RETURN QUERY
-  SELECT 
-    (STRING_TO_ARRAY(UNNEST(get_fasta_sequences_for_imgt_ids), E'\t'))[1] imgt_gene_name,
-	(STRING_TO_ARRAY(UNNEST(get_fasta_sequences_for_imgt_ids), E'\t'))[2] fasta_sequence
-  FROM
-    antibodies.get_fasta_sequences_for_imgt_ids(l_imgt_gene_names);
-END;
-$$
-LANGUAGE plpgsql
-STABLE
-SECURITY DEFINER;
-CREATE TABLE antibodies.imgt_gene_fasta_sequences AS SELECT * FROM antibodies.get_fasta_sequencesimgt_table();
-
-CREATE OR REPLACE FUNCTION antibodies.get_sequence_hash_id(p_ab_sequence TEXT)
-RETURNS TEXT
-AS
-$$
-DECLARE
-  l_sequence_hash_id TEXT;
-  l_ab_sequence TEXT;
-BEGIN
-  l_ab_sequence := antibodies.get_cleaned_amino_acid_sequence(p_ab_sequence);
-  
-  RETURN MD5(l_ab_sequence);
-  
-END;
-$$
-LANGUAGE plpgsql
-IMMUTABLE
-SECURITY DEFINER;
-
-
-CREATE OR REPLACE FUNCTION antibodies.add_mab_sequence(p_mab_identifier TEXT,
-											p_source_database_id TEXT,
-											p_source_database_url TEXT,
-											p_antibody_type TEXT,
-											p_heavy_chain_sequence TEXT,
-											p_light_chain_sequence TEXT)
-RETURNS JSONB
-AS
-$$
-DECLARE
-  l_retval TEXT := '{"load_outcome": "%s", "error_message": "%s"}';
-  l_sql_err_msg TEXT := 'NONE';
-  l_mab_full_sequence_has_id TEXT := antibodies.get_sequence_hash_id(CONCAT(p_heavy_chain_sequence, p_light_chain_sequence));
-  l_heavy_chain_sequence TEXT := antibodies.get_cleaned_amino_acid_sequence(p_heavy_chain_sequence);
-  l_light_chain_sequence TEXT := antibodies.get_cleaned_amino_acid_sequence(p_light_chain_sequence);
-BEGIN
-  
-  INSERT INTO antibodies.mab_sequences(mab_full_sequence_hash_id, 
-									   commonly_known_as, 
-									   source_database_id, 
-									   source_database_url, 
-									   antibody_type, 
-									   heavy_chain_sequence, 
-									   light_chain_sequence)
-    VALUES(l_mab_full_sequence_has_id,
-		  p_mab_identifier,
-		  p_source_database_id,
-		  p_source_database_url,
-		  p_antibody_type,
-		  l_heavy_chain_sequence,
-		  l_light_chain_sequence);
-  l_retval := FORMAT(l_retval, 'Loaded', l_sql_err_msg);
-  
-  RETURN l_retval::JSONB;
-  
-EXCEPTION
-   WHEN UNIQUE_VIOLATION THEN
-     l_retval := FORMAT(l_retval, 'Not Loaded', 'Ab sequence already in the database!');
-	 
-	 RETURN l_retval::JSONB;
-	 
-   WHEN OTHERS THEN
-     l_sql_err_msg := SQLERRM;
-	   l_sql_err_msg := REPLACE(l_sql_err_msg, '"', '|');
-	   l_retval := FORMAT(l_retval, 'Not loaded', l_sql_err_msg);
-	 
-	 RETURN l_retval;
-	 
-END;
-$$
-LANGUAGE plpgsql
-SECURITY DEFINER;
-
-
-
--- CDR determination
 CREATE OR REPLACE FUNCTION antibodies.get_cleaned_amino_acid_sequence(p_amino_acid_sequence TEXT, p_extra_allowable_chars TEXT DEFAULT NULL)
 RETURNS TEXT
 AS
@@ -222,6 +19,125 @@ IMMUTABLE
 SECURITY DEFINER;
 SELECT get_cleaned_amino_acid_sequence FROM antibodies.get_cleaned_amino_acid_sequence('PQVTLWQRPI VTIKIGGQLK EALLDTGADD TVLEEMSLPG KWKPKMIGGIX', 'X');
 
+-- Loading antibodies
+CREATE OR REPLACE FUNCTION antibodies.load_information(p_common_identifier TEXT,
+													   p_antibody_type TEXT,
+													   p_antibody_source TEXT,
+													   p_source_database_id TEXT,
+													   p_source_database_url TEXT)
+RETURNS BOOLEAN
+AS
+$$
+DECLARE
+ l_new_record_created BOOLEAN;
+BEGIN
+  INSERT INTO antibodies.information(common_identifier, antibody_type, antibody_source, source_database_id, source_database_url)
+    VALUES(p_common_identifier, p_antibody_type, p_antibody_source, p_source_database_id, p_source_database_url);
+  l_new_record_created := TRUE;
+  
+  RETURN l_new_record_created;
+  
+EXCEPTION
+  WHEN UNIQUE_VIOLATION THEN
+    l_new_record_created := FALSE;
+	
+	RETURN l_new_record_created;
+	
+END;
+$$
+LANGUAGE plpgsql
+SECURITY DEFINER;
+
+CREATE OR REPLACE FUNCTION antibodies.load_amino_acid_sequence(p_amino_acid_sequence_hash_id TEXT, p_amino_acid_sequence TEXT, p_chain_type TEXT)
+RETURNS BOOLEAN
+AS
+$$
+DECLARE
+  l_new_record_created BOOLEAN;
+BEGIN
+  INSERT INTO antibodies.amino_acid_sequences(amino_acid_sequence_id, amino_acid_sequence, chain_type)
+    VALUES(p_amino_acid_sequence_hash_id, p_amino_acid_sequence, p_chain_type);
+  l_new_record_created := TRUE;
+  
+  RETURN l_new_record_created;
+  
+EXCEPTION
+  WHEN UNIQUE_VIOLATION THEN
+    l_new_record_created := FALSE;
+	
+	RETURN l_new_record_created;
+
+END;
+$$
+LANGUAGE plpgsql
+SECURITY DEFINER;
+
+CREATE OR REPLACE FUNCTION antibodies.create_new_antibody_sequence_entry(p_common_identifier TEXT,
+																		 p_antibody_type TEXT,
+													                     p_antibody_source TEXT,
+													                     p_source_database_id TEXT,
+													                     p_source_database_url TEXT,
+																		 p_antibody_sequence TEXT,
+																		 p_chain_type TEXT)
+RETURNS TEXT
+AS
+$$
+DECLARE
+  l_common_identifier TEXT := TRIM(UPPER(p_common_identifier));
+  l_antibody_sequence TEXT := antibodies.get_cleaned_amino_acid_sequence(p_antibody_sequence);
+  l_antibody_sequence_hash_id TEXT := antibodies.get_sequence_hash_id(p_antibody_sequence);
+  l_retval TEXT := 'Successful upload of record for antibody identifier "%s" and antibody sequence hash ID "%s". New information row created: "%s". New sequence row created: "%s"';
+  l_new_information_record_created BOOLEAN;
+  l_new_sequence_record_created BOOLEAN;
+BEGIN
+  l_new_information_record_created := antibodies.load_information(l_common_identifier, p_antibody_type, p_antibody_source, p_source_database_id, p_source_database_url);
+  l_new_sequence_record_created := antibodies.load_amino_acid_sequence(l_antibody_sequence_hash_id, l_antibody_sequence, p_chain_type);
+  INSERT INTO antibodies.information_amino_acid_sequences(common_identifier, amino_acid_sequence_id)
+    VALUES(l_common_identifier, l_antibody_sequence_hash_id);
+  l_retval := FORMAT(l_retval, l_common_identifier, l_antibody_sequence_hash_id, l_new_information_record_created, l_new_sequence_record_created);
+  
+  RETURN l_retval;
+
+EXCEPTION
+  WHEN UNIQUE_VIOLATION THEN
+    l_retval := FORMAT('Duplicate entry for antibody identifier "%s" and sequence hash ID "%s".', l_common_identifier, l_antibody_sequence_hash_id);
+	
+	RETURN l_retval;
+	
+  WHEN OTHERS THEN
+    l_retval := FORMAT('ERROR: %s', SQLERRM);
+
+	RETURN l_retval;
+	
+END;
+$$
+LANGUAGE plpgsql
+SECURITY DEFINER;
+
+
+CREATE OR REPLACE FUNCTION antibodies.get_sequence_hash_id(p_ab_sequence TEXT)
+RETURNS TEXT
+AS
+$$
+DECLARE
+  l_sequence_hash_id TEXT;
+  l_ab_sequence TEXT;
+BEGIN
+  l_ab_sequence := antibodies.get_cleaned_amino_acid_sequence(p_ab_sequence);
+  
+  RETURN MD5(l_ab_sequence);
+  
+END;
+$$
+LANGUAGE plpgsql
+IMMUTABLE
+SECURITY DEFINER;
+
+
+
+
+
+-- CDR determination
 CREATE OR REPLACE FUNCTION antibodies.get_CDR_VL1_sequence(p_VL_amino_acid_sequence TEXT)
 RETURNS TEXT
 AS
@@ -328,7 +244,64 @@ SELECT antibodies.get_CDR_VH3_sequence('EVQLLESGGGLVQPGGSLRLSCAVSGFTFNSFAMSWVRQA
 
 
 
+-- Functions for IMGT and FASTA
+CREATE OR REPLACE FUNCTION antibodies.get_fasta_sequences_for_imgt_ids(p_imgt_ids TEXT[])
+RETURNS TEXT[]
+AS
+$$
+DECLARE
+  l_fasta_seqs TEXT[];
+  l_counter INTEGER := 1;
+  l_response TEXT;
+  imgt_id TEXT;
+BEGIN
+  FOREACH imgt_id IN ARRAY p_imgt_ids
+  LOOP
+    BEGIN
+	    l_response := bix_udfs.get_fasta_seq_as_aa_for_gene_synonym(imgt_id);
+	    l_response := CONCAT(imgt_id, E'\t', l_response);
+	    l_fasta_seqs[l_counter] := l_response;
+	    l_counter := l_counter + 1;
+	  EXCEPTION
+	    WHEN OTHERS THEN
+	      l_response := CONCAT(imgt_id, E'\t', 'ERROR');
+	      l_fasta_seqs[l_counter] := l_response;
+	      l_counter := l_counter + 1;
+	  END;
+  END LOOP;
+  
+  RETURN l_fasta_seqs;
+  
+END;
+$$
+LANGUAGE plpgsql
+STABLE
+SECURITY DEFINER;
+SELECT antibodies.get_fasta_sequences_for_imgt_ids(ARRAY['IGHV4-34', 'IGHV3-21', 'IGHV1-2']);
 
+CREATE OR REPLACE FUNCTION antibodies.get_fasta_sequencesimgt_table()
+RETURNS TABLE(imgt_gene_name TEXT, fasta_sequence TEXT)
+AS
+$$
+DECLARE
+  l_imgt_gene_names TEXT[];
+BEGIN
+  SELECT 
+    ARRAY_AGG(lu.imgt_gene_name) INTO l_imgt_gene_names 
+  FROM
+    antibodies.germline_adimab_imgt_lu lu;
+  RETURN QUERY
+  SELECT 
+    (STRING_TO_ARRAY(UNNEST(get_fasta_sequences_for_imgt_ids), E'\t'))[1] imgt_gene_name,
+	(STRING_TO_ARRAY(UNNEST(get_fasta_sequences_for_imgt_ids), E'\t'))[2] fasta_sequence
+  FROM
+    antibodies.get_fasta_sequences_for_imgt_ids(l_imgt_gene_names);
+END;
+$$
+LANGUAGE plpgsql
+STABLE
+SECURITY DEFINER;
+CREATE TABLE antibodies.imgt_gene_fasta_sequences AS SELECT * FROM antibodies.get_fasta_sequencesimgt_table();
 
 
 
--- antibody_db/antibody_udfs_documentor.sql
+++ antibody_db/antibody_udfs_documentor.sql
@@ -1,38 +1,30 @@
 SELECT create_function_comment_statement(
-  'antibodies.add_new_antibody_sequence',
-  ARRAY['TEXT', 'TEXT', 'TEXT'],
-  'Adds an antibody sequence with a name and optional metadata to the databaseand returns a JSONB response with information on the database operations performed.',
-  $$SELECT * FROM antibodies.add_new_antibody_sequence('test ab 1', 'EVQLLESGGGLVQ', '{' || CHR(34) || 'adimab_id' || CHR(34) || ': 686}')$$,
-  'The input sequence and antibody name are stripped of white space and changed to upper case before any INSERT is called. ' ||
-  'The function calls two INSERT statements, each within their own BEGIN..END blocks to allow fine-grained exception handling and reporting.  ' ||
-  'These two INSERT statements update the |aa_sequences| and |aa_sequence_metadata| tables. ' ||
-  'The target table of the INSERT for the sequence enforces: ' ||
-  '--a CHECK to ensure that only sequences with recognised single amino acid codes are accepted. ' ||
-  '--a UNIQUE constraint on the amino acid sequence by calculating an MD5 hash value for the sequence and using this as the primary key. ' ||
-  'If this function is called again with the same input sequence but a different antibody name, ' ||
-  'a row is added to the metadata table to register the new name but the |aa_sequences| table is NOT updated. ' ||
-  'This function will attempt to always return a JSONB response even if an uncaught exception is thrown.  ' ||
-  'The calling code needs to check the returned JSONB for a key named |ERROR| to determine if an exception was thrown. ');
-  
+	'antibodies.load_information',
+	ARRAY['TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT'],
+	'Attempt to insert a row into the |information| table and return TRUE if the insert succeeded otherwise return FALSE.',
+    $$SELECT load_information FROM antibodies.load_information(UPPER(TRIM('infliximab')), 'Chimeric human-murine IgG1', 'Commercial antibody', NULL, 'https://www.sigmaaldrich.com/content/dam/sigma-aldrich/docs/Sigma/Datasheet/10/msqc9dat.pdf')$$,
+	'This function is called as part of a multi-table insert by function |create_new_antibody_sequence_entry|. ' ||
+	'If called directly, then other inserts are needed to maintain referential integrity.');
+
+
 SELECT create_function_comment_statement(
-  'antibodies.get_sequence_hash_id',
-   ARRAY['TEXT'],
-  'Return the MD5 hash ID for a given amino acid sequence.',
-  $$SELECT antibodies.get_sequence_hash_id('EVQLVESGGGLVKPGGSLRLSCAASGFTFASYGMHWVRQAPGKGLEWVAVIWYDASTKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCARDLGYGDYAAHDYWGQGTLVTVSS');$$,
-  'This function will only calculate a hash ID for a text string that contains only valid sinbgle letter amino acid codes. ' ||
-  'This rule is enforced by its call to |antibodies.get_cleaned_amino_acid_sequence| and MD5 hash ID is calculated for the return value of this function.');
-  
-  
- SELECT create_function_comment_statement(
-  'antibodies.add_mab_sequence',
-   ARRAY['TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT'],
-  'Loads a Mab record to the table |antibodies.mab_sequences| and returns JSONB with details of the transaction.',
-  $$SELECT  add_mab_sequence FROM  antibodies.add_mab_sequence('Dupilumab', 'D10354', 'https://www.genome.jp/dbget-bin/www_bget?dr:D10354', 'Human IgG4', 'EVQLVESGGG LEQPGGSLRL SCAGSGFTFR DYAMTWVRQA PGKGLEWVSS ISGSGGNTYYADSVKGRFTI SRDNSKNTLY LQMNSLRAED TAVYYCAKDR LSITIRPRYY GLDVWGQGTTVTVSSASTKG PSVFPLAPCS RSTSESTAAL GCLVKDYFPE PVTVSWNSGA LTSGVHTFPAVLQSSGLYSL SSVVTVPSSS LGTKTYTCNV DHKPSNTKVD KRVESKYGPP CPPCPAPEFLGGPSVFLFPP KPKDTLMISR TPEVTCVVVD VSQEDPEVQF NWYVDGVEVH NAKTKPREEQFNSTYRVVSV LTVLHQDWLN GKEYKCKVSN KGLPSSIEKT ISKAKGQPRE PQVYTLPPSQEEMTKNQVSL TCLVKGFYPS DIAVEWESNG QPENNYKTTP PVLDSDGSFF LYSRLTVDKSRWQEGNVFSC SVMHEALHNH YTQKSLSLSL G', 'DIVMTQSPLS LPVTPGEPAS ISCRSSQSLL YSIGYNYLDW YLQKSGQSPQ LLIYLGSNRASGVPDRFSGS GSGTDFTLKI SRVEAEDVGF YYCMQALQTP YTFGQGTKLE IKRTVAAPSVFIFPPSDEQL KSGTASVVCL LNNFYPREAK VQWKVDNALQ SGNSQESVTE QDSKDSTYSLSSTLTLSKAD YEKHKVYACE VTHQGLSSPV TKSFNRGEC');$$,
-  'This function creates a new record in the |antibodies.mab_sequences| table if one does not already exist. ' ||
-  'Each added Mab is assigned a hash ID value calculated from the concatentation of the heavy and light chain sequence ' ||
-  'where the heavy chain is always first. If this hash ID already exists in the table, the load operation will not happen and ' ||
-  'the returned JSONB will indicate this. Regardless of the outcome, a JSONB string should always be returned that indicates either success ' ||
-  'or failure. The calling code can get the status from the JSONB |error_message| key.');
+	'antibodies.load_amino_acid_sequence',
+	ARRAY['TEXT', 'TEXT', 'TEXT'],
+	'Attempt to insert a row into the |amino_acid_sequences| table and return TRUE if the insert succeeded otherwise return FALSE.',
+    $$SELECT load_amino_acid_sequence FROM antibodies.load_amino_acid_sequence(antibodies.get_sequence_hash_id('DILLTQSPAILSVSPGERVSFSCRASQFVGSSIHWYQQRTNGSPRLLIKYASESMSGIPSRFSGSGSGTDFTLSINTVESEDIADYYCQQSHSWPFTFGSGTNLEVKRTVAAPSVFIFPPSDEQLKSGTASVVCLLNNFYPREAKVQWKVDNALQSGNSQESVTEQDSKDSTYSLSSTLTLSKADYEKHKVYACEVTHQGLSSPVTKSFNRGEC'), antibodies.get_cleaned_amino_acid_sequence('DILLTQSPAILSVSPGERVSFSCRASQFVGSSIHWYQQRTNGSPRLLIKYASESMSGIPSRFSGSGSGTDFTLSINTVESEDIADYYCQQSHSWPFTFGSGTNLEVKRTVAAPSVFIFPPSDEQLKSGTASVVCLLNNFYPREAKVQWKVDNALQSGNSQESVTEQDSKDSTYSLSSTLTLSKADYEKHKVYACEVTHQGLSSPVTKSFNRGEC'), 'L')$$,
+	'This function is called as part of a multi-table insert by function |create_new_antibody_sequence_entry|. ' ||
+	'If called directly, then other inserts are needed to maintain referential integrity.');	
 
 
- 
+SELECT create_function_comment_statement(
+	'antibodies.create_new_antibody_sequence_entry',
+	ARRAY['TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT'],
+	'Perform inserts on all relevant tables for a given antibody sequence and antibody information and return a text description of the operations performed or error generated',
+	$$SELECT create_new_antibody_sequence_entry FROM antibodies.create_new_antibody_sequence_entry('infliximab', 'Chimeric human-murine IgG1', 'Commercial monoclonal', NULL, 'https://www.sigmaaldrich.com/content/dam/sigma-aldrich/docs/Sigma/Datasheet/10/msqc9dat.pdf', 'EVKLEESGGGLVQPGGSMKLSCVASGFIFSNHWMNWVRQSP EKGLEWVAEIRSKSINSATHYAESVKGRFTISRDDSKSAVY LQMTDLRTEDTGVYYCSRNYYGSTYDYWGQGTTLTVSSAST KGPSVFPLAPSSKSTSGGTAALGCLVKDYFPEPVTVSWNSG ALTSGVHTFPAVLQSSGLYSLSSVVTVPSSSLGTQTYICNV NHKPSNTKVDKKVEPKSCDKTHTCPPCPAPELLGGPSVFLF PPKPKDTLMISRTPEVTCVVVDVSHEDPEVKFNWYVDGVEV HNAKTKPREEQYNSTYRVVSVLTVLHQDWLNGKEYKCKVSN KALPAPIEKTISKAKGQPREPQVYTLPPSRDELTKNQVSLT CLVKGFYPSDIAVEWESNGQPENNYKTTPPVLDSDGSFFLY SKLTVDKSRWQQGNVFSCSVMHEALHNHYTQKSLSLSPG ', 'H');$$,
+	'This is the function that should be used to create a new antibody entry in the database. ' ||
+	'It either directly or indirectly via calls to other functions performs inserts on three tables: |information|, |amino_acid_sequences| and the join table |information_amino_acid_sequences|. ' ||
+	'Inserts are performed so that referential integrity is maintained. ' ||
+	'Calling code can inspect the returned text to ascertain what operations were performed. ' ||
+	'Primary key violations in the join table |information_amino_acid_sequences| are reported ' ||
+	'and all other errors will result in a returned string beginning with |ERROR|. Calling code needs to check if an error has been generated.');
+	
