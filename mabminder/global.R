4227ea98670451a3d59ed49b6a943d06d4a626cd
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2018-06-19 15:38:44 +0100

    added

--- mabminder/global.R
+++ mabminder/global.R
@@ -1,16 +1,16 @@
 library(pool)
 library(DBI)
-
+library(tools)
 
 # Links:
 # https://stackoverflow.com/questions/24265980/reset-inputs-button-in-shiny-app#24269691
-
+# 192.168.49.15
 # Note: This is using the package RPostgres and NOT RPostgreSQL!!
 getPgConnection <- function(userName, password) {
   pool <- dbPool(
     drv = RPostgres::Postgres(),
     dbname = "mabminder",
-    host = "192.168.49.15",
+    host = "localhost",
     user = userName,
     port = 5432,
     password = password
@@ -33,5 +33,28 @@ createNewAntibodySequenceEntry <- function(pgConn, commonIdentifier, antibodyTyp
   loadResult <- DBI::dbGetQuery(pgConn, sql)
   
   return(loadResult)
+  
+}
+
+# Upload a file
+dirForUploadedFiles <- './uploaded_files'
+if(!dir.exists(dirForUploadedFiles)) {
+  dir.create(dirForUploadedFiles)
+}
+
+writeFileRecordToDB <- function(pgConn, chkSum, fromBasename, documentDescription) {
+  print("Called!")
+}
+
+storeFile <- function(pgConn, fromFullPath, fromBasename, documentDescription) {
+  print(fromBasename)
+  if(file.exists(file.path(dirForUploadedFiles, fromBasename))) {
+    file.remove(file.path(dirForUploadedFiles, fromBasename))
+  }
+  file.copy(fromFullPath, file.path(dirForUploadedFiles, fromBasename))
+  chkSum <- md5sum(file.path(dirForUploadedFiles, fromBasename))
+  writeFileRecordToDB(pgConn, chkSum, fromBasename, documentDescription)
+  
+  return(file.path(dirForUploadedFiles, fromBasename))
   
 }
